039464cc81d4e83aabedf8f5ff521d883feee456
mbaka bilal <mbakabilal.t@gmail.com>
2025-10-01 07:52:47 +0100

    configure socket io to use the same post

--- src/routes/share-location/share-location-websocket-gateway/share-location-websocket-gateway.gateway.ts
+++ src/routes/share-location/share-location-websocket-gateway/share-location-websocket-gateway.gateway.ts
@@ -13,7 +13,7 @@ import { SocketGuard } from 'src/guards/socket_guard';
 import { CleanData } from 'src/core/helpers/clean_data';
 import { ConnectedUsersService } from '../user/connected_users_service';
 
-@WebSocketGateway(5000, { transports: ['websocket'] })
+@WebSocketGateway({ transports: ['websocket'] })
 export class ShareLocationWebsocketGatewayGateway
     implements OnGatewayConnection, OnGatewayDisconnect
 {
